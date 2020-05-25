74b095096c13f14bae99644d3dbadd6bccb74b2e
Luis Benavides Rivera <L09_BENAVIDES@hotmail.com>
2020-05-25 12:32:03 -0600

    Fix Remaining Connection error

--- assets/conexion/conexion.php
+++ assets/conexion/conexion.php
@@ -1,5 +1,5 @@
 <?php
-	$mysqli=new mysqli("localhost","mechesdbuser","Jg8TeXl03Efc","mechesfermentsDB"); //servidor, usuario de base de datos, contraseña del usuario, nombre de base de datos
+	$mysqli=new mysqli("mysqldb-tienda.mysql.database.azure.com","dbadmin@mysqldb-tienda","Truecontrol12!","tienda"); //servidor, usuario de base de datos, contraseña del usuario, nombre de base de datos
 	
 	if(mysqli_connect_errno()){
 		echo 'Conexion Fallida : ', mysqli_connect_error();
